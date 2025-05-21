f76963a7a3340d79fbbb5c147bbb63a41b07249c
Willinki <d.badalotti@campus.unimib.it>
2025-05-21 18:50:40 +0200

    fix: comma

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='src',
-    version='0.1.0',
+    name="src",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        'hydra-core>=1.3',
-        'torch>=2.2',
-        'torchvision>=0.22',
-        'pytorch-lightning>=2.5'
-        'hydra_submitit_launcher>=1.1.0'
+        "hydra-core>=1.3",
+        "torch>=2.2",
+        "torchvision>=0.22",
+        "pytorch-lightning>=2.5",
+        "hydra_submitit_launcher>=1.1.0",
     ],
     extras_require={
-        'log': ['wandb>=0.19'],
+        "log": ["wandb>=0.19"],
     },
-    python_requires='>=3.11, <4',
-    author='Davide Badalotti',
-    author_email='davide.badalotti@unibocconi.it',
-    description='Simple project',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+    python_requires=">=3.11, <4",
+    author="Davide Badalotti",
+    author_email="davide.badalotti@unibocconi.it",
+    description="Simple project",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
 )
-
