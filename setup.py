ca1c3f416611ded8430407c05bc08d18518f815e
Willinki <d.badalotti@campus.unimib.it>
2025-05-21 15:04:55 +0200

    chore: starting out

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'hydra-core>=1.3',
+        'torch>=2.2',
+        'torchvision>=0.22',
+    ],
+    extras_require={
+        'log': ['wandb>=0.19'],
+    },
+    python_requires='>=3.11, <4',
+    author='Davide Badalotti',
+    author_email='davide.badalotti@unibocconi.it',
+    description='Simple project',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
+
